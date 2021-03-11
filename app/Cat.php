d26b94a7ed9f61b7eccbf28a9911d362f6f638c9
mariemsaadaoui <mariem.saadaoui1993@gmail.com>
2021-03-11 11:50:07 +0100

    modifie le modèle Cat

--- app/Cat.php
+++ app/Cat.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cat extends Model
 {
-    /*protected $fillable = [
+    protected $fillable = [
         'image_url', 'image_id','votes'
-    ];*/
-    protected $guarded = [];
+    ];
+    //protected $guarded = [];
 }
